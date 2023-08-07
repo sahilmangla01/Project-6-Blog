2d4ed5b3331425ed7fe665b4656cd026c9d29920
sahilmangla01 <manglasahil9050@gmail.com>
2023-08-07 12:03:40 +0530

    final

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -36,11 +36,11 @@ const Content = ()=>{
                 <div className="uptop">
                 
                 <div className="display ">
-                <img className="clapping" src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:b8a8258d-7b0b-4042-9818-a758226f5b5c;revision=0?component_id=cf55aaa5-c61c-40c7-8fb8-5622c86d93bb&api_key=CometServer1&access_token=1690817370_urn%3Aaaid%3Asc%3AUS%3Ab8a8258d-7b0b-4042-9818-a758226f5b5c%3Bpublic_ad3f691f7a3c36763f0dae78c710dd2898b66c33" alt="clap"/>
+                <img className="clapping" src="https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/clap-icon.png" alt="clap"/>
                 <h3>9.3k claps</h3></div>
                 
                 <div className="display ">
-                <img className="clapping" src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:b8a8258d-7b0b-4042-9818-a758226f5b5c;revision=0?component_id=178e3f84-dd79-4832-8c69-7926133e32ab&api_key=CometServer1&access_token=1690817370_urn%3Aaaid%3Asc%3AUS%3Ab8a8258d-7b0b-4042-9818-a758226f5b5c%3Bpublic_ad3f691f7a3c36763f0dae78c710dd2898b66c33" alt="clap"/>
+                <img className="clapping" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXoXe1wmQI1Z325EDgNtLngLvKxw6Efy2dDvih6aqJw9OZfU_hhnelADzFV-XdvPlriXY&usqp=CAU" alt="clap"/>
                 <h3>Share </h3>
                 </div>
                 
@@ -53,7 +53,7 @@ const Content = ()=>{
                 <div className="content__user">
                     <img className="icons" src="https://webstockreview.net/images/facebook-instagram-twitter-icons-png-12.png" alt="icons"/>
                     <div className="content__profile">
-                        <img src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:b8a8258d-7b0b-4042-9818-a758226f5b5c;revision=0?component_id=5648d0b6-d5e3-4f90-a1b0-6156c92a7695&api_key=CometServer1&access_token=1690817370_urn%3Aaaid%3Asc%3AUS%3Ab8a8258d-7b0b-4042-9818-a758226f5b5c%3Bpublic_ad3f691f7a3c36763f0dae78c710dd2898b66c33" alt="profile"/>
+                        <img className="profileIcon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsINewnG4nrTvE5ndRNUzUrRU5dTqWknJGzQ&usqp=CAU" alt="profile"/>
                         <div className="userName"><div>Sahil Mangla</div>
                             <h5>Aug 01, 2023</h5>
                         </div>
@@ -63,13 +63,13 @@ const Content = ()=>{
                 <p className="content__text">{content[id-1].long_description}</p>
                
                <div className="display">
-               <img className="clapping" src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:b8a8258d-7b0b-4042-9818-a758226f5b5c;revision=0?component_id=cf55aaa5-c61c-40c7-8fb8-5622c86d93bb&api_key=CometServer1&access_token=1690817370_urn%3Aaaid%3Asc%3AUS%3Ab8a8258d-7b0b-4042-9818-a758226f5b5c%3Bpublic_ad3f691f7a3c36763f0dae78c710dd2898b66c33" alt="clap"/>
+               <img className="clapping" src="https://uxwing.com/wp-content/themes/uxwing/download/hand-gestures/clap-icon.png" alt="clap"/>
                 <h3>9.3k claps</h3>
                </div>
                
 
                 <div className="content__profile profile2">
-                        <img src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:b8a8258d-7b0b-4042-9818-a758226f5b5c;revision=0?component_id=5648d0b6-d5e3-4f90-a1b0-6156c92a7695&api_key=CometServer1&access_token=1690817370_urn%3Aaaid%3Asc%3AUS%3Ab8a8258d-7b0b-4042-9818-a758226f5b5c%3Bpublic_ad3f691f7a3c36763f0dae78c710dd2898b66c33" alt="profile"/>
+                <img className="profileIcon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsINewnG4nrTvE5ndRNUzUrRU5dTqWknJGzQ&usqp=CAU" alt="profile"/>
                         <div className="userName2">
                             <h5>Written By</h5>
                             <div>Sahil Mangla</div>
